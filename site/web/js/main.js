c71e513e0500366ce85605324bfba4b8d15b75ea
Cory Simmons <csimmonswork@gmail.com>
2014-12-01 21:41:07 -0500

    Clean up stream page

--- site/web/js/main.js
+++ site/web/js/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
+  var resizeChatHeight = function() {
+    $('.stream-chat').height($('.streamer').height());
+  };
 
+  setTimeout(resizeChatHeight, 500);
+
+  var didResize = false;
+  $(window).resize(function() {
+    didResize = true;
+  });
+  setInterval(function() {
+    if(didResize) {
+      resizeChatHeight();
+      didResize = false;
+    }
+  }, 250);
 
 });
